f448190823dbb261c797a449371ea08363597969
Rajiv Kumbhakar <rajivnh@yahoo.com>
2023-07-15 12:50:32 +0530

    Added Config for static content whitelist

--- src/main/java/com/tutorial/config/SecurityConfig.java
+++ src/main/java/com/tutorial/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.tutorial.config;
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,8 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
                 .requestMatchers("/authenticate/**").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers(toH2Console()).permitAll()                		
+                .requestMatchers(toH2Console()).permitAll()     
                 .anyRequest().authenticated());
         
         http.sessionManagement((session) ->  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -35,5 +36,13 @@ public class SecurityConfig {
         http.headers(header -> header.frameOptions(config -> config.sameOrigin()));
         
         return http.build();
+    }
+    
+    @Bean
+    public WebSecurityCustomizer apiStaticResources() {
+        return (web) -> web.ignoring()
+        		.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        		.and().ignoring()
+        		.requestMatchers("/*.html");
     }
 }
